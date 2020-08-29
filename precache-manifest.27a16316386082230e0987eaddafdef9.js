ae81dbbd81d6c230929726ece4dce78d3a1d6032
unknown <susnatadj.rocks@gmail.com>
2020-08-29 22:34:48 +0530

    Updates

---
+++ precache-manifest.27a16316386082230e0987eaddafdef9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d865b6eb2f3d2be2fd2c5b72d747b8f5",
+    "url": "/Quiz-Flashcards/index.html"
+  },
+  {
+    "revision": "725a35cd89ae7e9d9fd7",
+    "url": "/Quiz-Flashcards/static/css/main.0f525f2b.chunk.css"
+  },
+  {
+    "revision": "1b1ad7614774056ce6c9",
+    "url": "/Quiz-Flashcards/static/js/2.6389a2a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quiz-Flashcards/static/js/2.6389a2a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "725a35cd89ae7e9d9fd7",
+    "url": "/Quiz-Flashcards/static/js/main.030ac732.chunk.js"
+  },
+  {
+    "revision": "1b6800755e4d98a69397",
+    "url": "/Quiz-Flashcards/static/js/runtime-main.f4c7259e.js"
+  }
+]);
